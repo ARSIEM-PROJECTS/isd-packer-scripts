8328c802dac6d0644903f1fb7c70282962ea6e91
mdrgon1 <mdrgon1@umbc.edu>
2022-07-06 12:23:20 -0400

    added -y to git install

--- Windows/windows-install-git.ps1
+++ Windows/windows-install-git.ps1
@@ -7,7 +7,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install git.install
+choco install git.install -y
 
 # edit registry settings
 REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v SecurityLayer /t REG_DWORD /d 0x00000001 /f
